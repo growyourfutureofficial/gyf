00761e1fd58bf80144953def33c5d3146d06252d
growyourfutureofficial <growyourfutureofficial@gmail.com>
2026-09-19 22:47:20 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 // ==========================================
 // 1. OFFLINE CACHING ENGINE (No Internet Dinosaur Fix)
 // ==========================================
-const CACHE_NAME = 'buildmoney-offline-cache-v14';
+const CACHE_NAME = 'buildmoney-offline-cache-v15';
 const urlsToCache = [
     './',
     './index.html',
